33322fbb8b7d29626aca8058344a7ba3fcd4f0f9
galenomoon <galeno.dev@gmail.com>
2023-12-15 15:10:02 -0300

    Add error handling to UsersController methods

--- src/users/users.controller.ts
+++ src/users/users.controller.ts
@@ -1,4 +1,14 @@
-import { Controller, Get, Post, Body, Patch, Param, Delete } from '@nestjs/common';
+import {
+  Controller,
+  Get,
+  Post,
+  Body,
+  Patch,
+  Param,
+  Delete,
+  HttpException,
+  HttpStatus,
+} from '@nestjs/common';
 import { UsersService } from './users.service';
 import { CreateUserDto } from './dto/create-user.dto';
 import { UpdateUserDto } from './dto/update-user.dto';
@@ -8,27 +18,82 @@ export class UsersController {
   constructor(private readonly usersService: UsersService) {}
 
   @Post()
-  create(@Body() createUserDto: CreateUserDto) {
-    return this.usersService.create(createUserDto);
+  async create(@Body() createUserDto: CreateUserDto) {
+    try {
+      return await this.usersService.create(createUserDto);
+    } catch (error) {
+      throw new HttpException(
+        {
+          status: error.status,
+          error: error.message,
+        },
+        HttpStatus.FORBIDDEN,
+        { cause: error },
+      );
+    }
   }
 
   @Get()
-  findAll() {
-    return this.usersService.findAll();
+  async findAll() {
+    try {
+      return await this.usersService.findAll();
+    } catch (error) {
+      throw new HttpException(
+        {
+          status: error.status,
+          error: error.message,
+        },
+        HttpStatus.FORBIDDEN,
+        { cause: error },
+      );
+    }
   }
 
   @Get(':id')
-  findOne(@Param('id') id: string) {
-    return this.usersService.findOne(+id);
+  async findOne(@Param('id') id: string) {
+    try {
+      return await this.usersService.findOne(+id);
+    } catch (error) {
+      throw new HttpException(
+        {
+          status: error.status,
+          error: error.message,
+        },
+        HttpStatus.FORBIDDEN,
+        { cause: error },
+      );
+    }
   }
 
   @Patch(':id')
-  update(@Param('id') id: string, @Body() updateUserDto: UpdateUserDto) {
-    return this.usersService.update(+id, updateUserDto);
+  async update(@Param('id') id: string, @Body() updateUserDto: UpdateUserDto) {
+    try {
+      return await this.usersService.update(+id, updateUserDto);
+    } catch (error) {
+      throw new HttpException(
+        {
+          status: error.status,
+          error: error.message,
+        },
+        HttpStatus.FORBIDDEN,
+        { cause: error },
+      );
+    }
   }
 
   @Delete(':id')
-  remove(@Param('id') id: string) {
-    return this.usersService.remove(+id);
+  async remove(@Param('id') id: string) {
+    try {
+      return await this.usersService.remove(+id);
+    } catch (error) {
+      throw new HttpException(
+        {
+          status: error.status,
+          error: error.message,
+        },
+        HttpStatus.FORBIDDEN,
+        { cause: error },
+      );
+    }
   }
 }
--- src/users/users.service.ts
+++ src/users/users.service.ts
@@ -9,6 +9,13 @@ export class UsersService {
 
   async create(createUserDto: CreateUserDto) {
     const { email, username, password } = createUserDto;
+
+    const emailExists = await this.findByEmail(email);
+    if (emailExists) throw new Error('Email already exists');
+
+    const usernameExists = await this.findByUsername(username);
+    if (usernameExists) throw new Error('Username already exists');
+
     const user = await this.prisma.user.create({
       data: {
         email,
@@ -26,6 +33,16 @@ export class UsersService {
 
   async findOne(id: number) {
     const user = await this.prisma.user.findUnique({ where: { id } });
+    return user;
+  }
+
+  async findByEmail(email: string) {
+    const user = await this.prisma.user.findUnique({ where: { email } });
+    return user;
+  }
+
+  async findByUsername(username: string) {
+    const user = await this.prisma.user.findUnique({ where: { username } });
     return user;
   }
 
